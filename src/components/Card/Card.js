bf7568e1acb83f6cf1507d73b7958258762f0aad
Dcarolina <diacvelasquez11@misena.edu.co>
2022-09-23 19:07:28 -0500

    Examen

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({results}) => {
+    let display;
+
+    if (results) {
+        display = results.map((x) => {
+            let { id, image, name, status, location } = x;
+
+    return (
+        <div key={id} className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark">
+        </div>
+    );
+    });
+    }
